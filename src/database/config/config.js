8a8963d9c0a8ef8b9550b330270ad571e9aa0cc1
hikmani <syarifulfajar017@gmail.com>
2026-02-22 17:07:17 +0700

    setup Dockerfile and docker-compose.yml

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    logging: false,
+  },
+  test: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    logging: false,
   },
   production: {
-    url: process.env.DATABASE_URL,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    logging: false,
   },
 };
